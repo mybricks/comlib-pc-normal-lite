6e7ade8a78f166244e999794991fc4a4e5a7bc19
moyukai05 <moyukai05@kuaishou.com>
2026-01-27 19:49:50 +0800

    feat:update

--- src/icon/toReact.ts
+++ src/icon/toReact.ts
@@ -11,11 +11,9 @@ export default function ({ data }) {
     delete style.styleEditorUnfold;
   }
 
-  // 判断图标类型：Ant Design 图标 or 自定义 SVG/HTML
-//   const isAntdIcon = data.icon && /^[A-Z]/.test(data.icon);
 
     // 使用 Ant Design 图标 - 关键：用组件变量名，不是字符串
-    let  jsx = `<HomeOutlined style={${getObjectStr(style)}} />`;
+    let  jsx = `<${data.icon} style={${getObjectStr(style)}} />`;
 
   return {
     imports: [
@@ -25,7 +23,7 @@ export default function ({ data }) {
       },
     {
         from: '@ant-design/icons',
-        coms: ['HomeOutlined'],
+        coms: [data.icon],
       }
     ],
     jsx,
