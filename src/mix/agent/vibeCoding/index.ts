2ec465d80287b73b497481b76ba8f8f8a20cd2ca
leon-js <42924541+leon-js@users.noreply.github.com>
2026-03-17 20:14:38 +0800

    update

--- src/mix/agent/vibeCoding/index.ts
+++ src/mix/agent/vibeCoding/index.ts
@@ -412,11 +412,10 @@ export default function ({ context }) {
       const themesContent = (() => {
         try {
           const themes = aiComParams?.data?.themes ?? [];
-          return '- 设计风格：' + 
-          '\n  ui设计参考以下主题变量，css变量已经自动注入页面，直接使用变量即可，禁止重复定义。' + 
+          return '- 设计风格：' + (themes?.length ? '\n  ui设计参考以下主题变量，css变量已经自动注入页面，直接使用变量即可，禁止重复定义。' + 
           themes.reduce((pre, cur) => {
             return pre + `\n  - ${cur.title}： ${cur.propertyName}: ${cur.value}`
-          }, "") || "\n  当前项目没有主题变量，根据需求自由发挥即可。"
+          }, "") : '\n  当前项目没有定义主题变量，禁止创造变量，风格根据需求自由发挥即可')
         } catch {
           return '';
         }
