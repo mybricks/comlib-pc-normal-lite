8ede24409389bd7aacc1645fa7158b70f9b37147
liubiao03 <liubiao03@kuaishou.com>
2026-03-30 20:40:09 +0800

    feat: update

--- src/mix/editors.tsx
+++ src/mix/editors.tsx
@@ -5,6 +5,7 @@ import consoleViewCss from "./lowcodeView/console/index.lazy.less"
 import versionViewCss from "./lowcodeView/version/index.lazy.less"
 import treeViewCss from "./lowcodeView/tree/index.lazy.less"
 import context from "./context";
+import { generateCodeStructure } from '../utils/code-export/structure-generator';
 import {ANTD_KNOWLEDGES_MAP, ANTD_ICONS_KNOWLEDGES_MAP} from "./knowledges";
 import {parseLess, stringifyLess} from "./utils/transform/less";
 import {deepClone} from "./utils/normal";
@@ -387,10 +388,40 @@ const genResizer = () => {
   }
 }
 
+function registerResourcesCode(comId: string, comName: string) {
+  if (!(window as any)._forApp_) {
+    (window as any)._forApp_ = {};
+  }
+  const forApp = (window as any)._forApp_;
+  forApp[comId] = { id: comId, name: comName, getFiles: (type: 'application' | 'component') => {
+    const aiComParams = context.getAiComParams(comId);
+    if (!aiComParams?.data) return null;
+    return generateCodeStructure(aiComParams.data, { type });
+  }};
+  forApp._getResourcesCode_ = (type: 'application' | 'component') => {
+    const result: Array<{ id: string; name: string; files: any[] }> = [];
+    for (const key of Object.keys(forApp)) {
+      if (key.startsWith('_')) continue;
+      try {
+        const instance = forApp[key];
+        const files = instance.getFiles(type);
+        if (files) {
+          result.push({ id: instance.id, name: instance.name, files });
+        }
+      } catch (e) {
+        console.error('[_getResourcesCode_] 实例导出失败', key, e);
+      }
+    }
+    return result;
+  };
+}
+
 export default function (props: Props, actions: Actions, ...args) {
   if (!props?.data || !props?.id) {
     return {};
   }
+
+  registerResourcesCode(props.id, props.name || props.id);
 
   const {data, isLowCodeMode} = props;
   const focusAreaConfigs: any = {};
