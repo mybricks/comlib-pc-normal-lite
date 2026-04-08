f32f8893b0567aa683d4799e3867bdd43444553d
leon-js <42924541+leon-js@users.noreply.github.com>
2026-04-08 15:06:36 +0800

    update

--- src/utils/code-export/codeTransform.ts
+++ src/utils/code-export/codeTransform.ts
@@ -2,7 +2,7 @@ const tramsform = (code) => {
   return code
     .replace(/from\s+['"]mybricks['"]/g, "from '@mybricks/ai-render'")
     .replace(/(from\s+['"][^'"]+)\.less(['"]\s*)/g, '$1.module.less$2')
-    .replace('@PopupVisible', '');
+    .replace(/@PopupVisible/g, '');
 }
 
 export { tramsform }
