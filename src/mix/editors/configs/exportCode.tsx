0da52d845b7903643eb694060ad5e647fbfe9010
moyukai05 <moyukai05@kuaishou.com>
2026-04-23 17:50:45 +0800

    save: 注释掉themes编辑器

--- src/mix/editors/configs/exportCode.tsx
+++ src/mix/editors/configs/exportCode.tsx
@@ -30,21 +30,21 @@ export function buildExportCodeConfig(props: Props) {
     //     },
     //   ],
     // },
-    {
-      type: 'themes',
-      value: {
-        get(params: any) {
-          if (params.data._themesModified) {
-            return params.data.themes;
-          }
-          const projectThemes = context.projectConfig.themes;
-          return (projectThemes && projectThemes.length > 0) ? projectThemes : params.data.themes;
-        },
-        set(params: any, themes: any) {
-          params.data._themesModified = true;
-          params.data.themes = themes;
-        },
-      },
-    },
+    // {
+    //   type: 'themes',
+    //   value: {
+    //     get(params: any) {
+    //       if (params.data._themesModified) {
+    //         return params.data.themes;
+    //       }
+    //       const projectThemes = context.projectConfig.themes;
+    //       return (projectThemes && projectThemes.length > 0) ? projectThemes : params.data.themes;
+    //     },
+    //     set(params: any, themes: any) {
+    //       params.data._themesModified = true;
+    //       params.data.themes = themes;
+    //     },
+    //   },
+    // },
   ];
 }
