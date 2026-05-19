779f0b7fc55faf98aa4f7145d54cfabe750a0bfa
leon-js <42924541+leon-js@users.noreply.github.com>
2026-05-19 21:44:08 +0800

    update

--- src/mix/runtime.edit.tsx
+++ src/mix/runtime.edit.tsx
@@ -33,8 +33,8 @@ const dataCompatible = (props) => {
       }
     }
 
-    if (!data.version || data.version < 5) {
-      data.version = 5
+    if (!data.version || data.version < 6) {
+      data.version = 6
       const readme = data.files.find((file) => file.fileName === "README.md")
       if (readme?.source) {
         context.updateFile(id, { fileName: "README.md", content: decodeURIComponent(readme.source) })
